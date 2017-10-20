b33f179e9621020d2d2a0f8e9113bc3739af5a3d
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-10-20 14:29:29 -0700

    Initial commit

---
+++ CommonObjects/Campaign.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public class Campaign {
+    public List<MissionParameters> Missions;
+    public string campaignName;
+
+    public Campaign()
+    {
+        campaignName = "New Campaign";
+        this.Missions = new List<MissionParameters>();
+    }
+
+    public void NewMission()
+    {
+        Missions.Add(new MissionParameters());
+    }
+    public void Load(Stream file)
+    // Load Player data from disk
+    {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            Campaign data = (Campaign)bf.Deserialize(file);
+            file.Close();
+
+        // Assign values to object in memory
+        Missions = data.Missions;
+        campaignName = data.campaignName;
+    }
+    public void Save(FileStream file)
+    // Load Player data from disk
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        Campaign data = this;
+
+        bf.Serialize(file, data);
+        file.Close();
+
+        // Assign values to object in memory
+    }
+}
+
+
+
---
+++ CommonObjects/MissionParameters.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MissionParameters {
+
+    public string DestinationName;
+    public float DistanceToDestination;
+
+    public string SystemPreview;
+    public string SystemImage;
+
+    public int NumberOfProblems;
+    public int NumberOfStars;
+
+    public string ConstellationName;
+    public string ConstellationInfo;
+    public string ConstellationImage;
+
+    public string BriefingText;
+    public string DiscoveryInfoText;
+    public string SystemDataText;
+
+    public string MissionPatch;
+
+    public string operation;
+    public int term0Upper;
+    public int term0Lower;
+    public int term1Upper;
+    public int term1Lower;
+
+    public MissionParameters()
+    {
+        this.operation = "+";
+        this.term0Upper = 9;
+        this.term0Lower = 0;
+        this.term1Upper = 9;
+        this.term1Lower = 0;
+
+        this.DestinationName = "Unknown System";
+        this.DistanceToDestination = 0f;
+
+        this.NumberOfProblems = 20;
+        this.NumberOfStars = 99;
+
+        this.ConstellationName = "Unknown Constellation";
+        this.ConstellationInfo = "No Data Available";
+        this.BriefingText = "This is uncharted territory we have no intelligence for you";
+        this.DiscoveryInfoText = "Your Sensors are malfunctioning";
+        this.SystemDataText = "No Data Available";
+    }
+}
+
